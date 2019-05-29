fd3d2fbcea2720d3464494f673ab376b38a4be8f
Piotr Oleskiewicz <piotr@oleskiewi.cz>
2019-05-29 19:11:13 +0100

    implement ishigami f & sobol indices

---
+++ sa-analyse-saltelli.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sysexits.h>
+#include <math.h>
+#include <gsl/gsl_blas.h>
+
+#include "util.h"
+
+void
+sa_analyse_saltelli(gsl_vector *Y, int n, int d) {
+	for(int i = 0; i < d; i++) {
+		double s = 0.0, st = 0.0;
+		for(int j = 0; j < n; j++) {
+			double a = gsl_vector_get(Y, j),
+			       b = gsl_vector_get(Y, j + n),
+			       ab = gsl_vector_get(Y, j + n + (n*(i+1)));
+
+			s += b * (ab - a);
+			st += pow(a - ab, 2.0);
+		}
+
+		s /= (double)n;
+		st /= 2.0 * (double)n;
+
+		printf("%f\t%f\n", s, st);
+	}
+}
+
+int
+main(int argc, char **argv) {
+	if(argc != 3) {
+		fprintf(stderr, "usage: %s n d\n", argv[0]);
+		return EX_USAGE;
+	}
+	int n = atoi(argv[1]), d = atoi(argv[2]);
+	gsl_vector *Y = gsl_vector_alloc(n * (2 + d));
+	if(gsl_vector_fscanf(stdin, Y) != 0) {
+		fprintf(stderr, "error: not read correctly\n");
+		return 1;
+	}
+
+	sa_analyse_saltelli(Y, n, d);
+
+	return 0;
+}
---
+++ sa-eval-ishigami.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sysexits.h>
+#include <math.h>
+
+double
+ishigami(double *x, double a, double b) {
+	return sin(x[0]) + a * pow(sin(x[1]), 2.0) + b * pow(x[2], 4.0) * sin(x[0]);
+}
+
+int
+main(int argc, char **argv) {
+	if(argc != 3) {
+		fprintf(stderr,
+				"usage: %s a b\n"
+				"\ty = sin(x[0]) + a * x[1]**2 + b * x[2]**4 * sin(x[0])\n",
+				argv[0]);
+		return EX_USAGE;
+	}
+	double a = atof(argv[1]), b = atof(argv[2]);
+	double x[3];
+
+	while(scanf("%lf %lf %lf", &x[0], &x[1], &x[2]) == 3) {
+		printf("%lf\n", ishigami(x, a, b));
+	}
+
+	return 0;
+}
